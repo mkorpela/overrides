50a02ee2937306bc744f71e4cfaf8dd0f7d32015
Mikko Korpela <mikko.korpela@gmail.com>
2015-05-29 23:25:21 -0700

    cleanup

--- overrides/overrides.py
+++ overrides/overrides.py
@@ -48,30 +48,26 @@ def overrides(method):
     :raises  AssertionError if no match in super classes for the method name
     :return  method with possibly added (if the method doesn't have one) docstring from super class
     """
-    frame = sys._getframe(2)
-    for super_class in _get_base_classes(frame.f_code, frame.f_lasti, method.__globals__):
+    for super_class in _get_base_classes(sys._getframe(2), method.__globals__):
         if hasattr(super_class, method.__name__):
             if not method.__doc__:
                 method.__doc__ = getattr(super_class, method.__name__).__doc__
             return method
     raise AssertionError('No super class method found for "%s"' % method.__name__)
 
-def _get_base_classes(f_code, f_lasti, namespace):
-    return [_get_base_class(class_name, namespace) for class_name in _get_base_class_names(f_code, f_lasti)]
+def _get_base_classes(frame, namespace):
+    return [_get_base_class(class_name_components, namespace) for class_name_components in _get_base_class_names(frame)]
 
-def _get_base_class_names(co, lasti=-1):
+def _get_base_class_names(frame):
     """Get baseclass names from the code object"""
+    co, lasti = frame.f_code, frame.f_lasti
     code = co.co_code
-    n = len(code)
     i = 0
     extended_arg = 0
-    stop = False
     extends = []
-    while not stop and i < n:
+    while i <= lasti:
         c = code[i]
         op = ord(c)
-        if i == lasti:
-            stop = True
         i += 1
         if op >= dis.HAVE_ARGUMENT:
             oparg = ord(code[i]) + ord(code[i+1])*256 + extended_arg
@@ -88,20 +84,19 @@ def _get_base_class_names(co, lasti=-1):
                 if dis.opname[op] == 'LOAD_ATTR':
                     extends.append(('attr', co.co_names[oparg]))
     items = []
-    previous_item = None
+    previous_item = []
     for t, s in extends:
         if t == 'name':
             if previous_item:
                 items.append(previous_item)
-            previous_item = s
+            previous_item = [s]
         else:
-            previous_item += '.' + s
+            previous_item += [s]
     if previous_item:
         items.append(previous_item)
     return items
 
-def _get_base_class(class_name, namespace):
-    components = class_name.split('.')
+def _get_base_class(components, namespace):
     obj = namespace[components[0]]
     for component in components[1:]:
         obj = getattr(obj, component)
