c58014ba479cf942f6f7607c1b668bd391edd8cc
dror <dror@intensix.com>
2016-04-20 14:41:21 +0300

    allow import in python 2

--- overrides/__init__.py
+++ overrides/__init__.py
@@ -1,2 +1,9 @@
-from overrides.overrides import overrides
-from overrides.overrides import __VERSION__
+import sys
+
+if sys.version < 3:
+    from overrides import overrides
+    from overrides import __VERSION__
+else:
+    from overrides.overrides import overrides
+    from overrides.overrides import __VERSION__
+
