1371ffcfe1f195d0a35b2656c94937c16651207d
Keunhong Lee <dlrmsghd@gmail.com>
2016-06-04 02:17:32 +0900

    Add final decorator and keyworkds

---
+++ tests/test_somefinalpackage.py
@@ -0,0 +1,12 @@
+from overrides import final
+
+
+class SomeClass(object):
+
+    def somewhat_fun_method(self):
+        """LULZ"""
+        return 'LOL'
+
+    @final
+    def somewhat_finalized_method(self):
+        return "some_final"
