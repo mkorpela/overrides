3067112948d4f69faded3ca620bb7fde3841cd38
Mikko Korpela <mikko.korpela@gmail.com>
2017-05-22 21:48:16 +0300

    python 3.6 support

--- setup.py
+++ setup.py
@@ -24,5 +24,6 @@ setup(name='overrides',
           'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6'
           ]
       )
