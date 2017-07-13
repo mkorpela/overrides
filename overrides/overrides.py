840c37e3b9b87780133a19ec39894fcf11e8a00e
Joel Grus <joelgrus@gmail.com>
2017-07-13 10:33:30 -0700

    allow overrides for builtins

--- overrides/overrides.py
+++ overrides/overrides.py
@@ -156,7 +156,10 @@ def _get_base_class_names(frame):
 
 
 def _get_base_class(components, namespace):
-    obj = namespace[components[0]]
+    try:
+        obj = namespace[components[0]]
+    except KeyError:
+        obj = namespace["__builtins__"][components[0]]
     for component in components[1:]:
         obj = getattr(obj, component)
     return obj
