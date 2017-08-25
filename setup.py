c563792ca81b5cffcca1cc343bf1fe1ed5c2b8de
Mikko Korpela <mikko.korpela@gmail.com>
2017-08-25 09:40:47 +0300

    update to version 1.9

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ address = name.lower().replace(' ', '.')+chr(64)+'gmail.com'
 desc = 'A decorator to automatically detect mismatch when overriding a method.'
 
 setup(name='overrides',
-      version='1.8',
+      version='1.9',
       description=desc,
       author=name,
       author_email=address,
