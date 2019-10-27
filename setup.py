58dae5e883f6c48c5fbc86b539c977088a6c5848
Mikko Korpela <mikko.korpela@gmail.com>
2019-10-27 18:54:35 +0200

    remove Python 3.4

--- setup.py
+++ setup.py
@@ -22,7 +22,6 @@ setup(name='overrides',
           'Intended Audience :: Developers',
           'Natural Language :: English',
           'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6'
           ]
