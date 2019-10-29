676e22a8a0805750d70da8ef03339f9ffcccb456
Tudor Masek <tmasek@gmail.com>
2019-10-29 15:48:14 -0700

    python 2(.7 at least) support

--- overrides/__init__.py
+++ overrides/__init__.py
@@ -4,6 +4,7 @@ if sys.version < '3':
     from overrides import overrides
     from final import final
     from overrides import __VERSION__
+    from enforce27 import EnforceOverrides
 else:
     from overrides.overrides import overrides
     from overrides.final import final
--- tests/test_enforce.py
+++ tests/test_enforce.py
@@ -1,48 +1,45 @@
-import sys
+import unittest
+from overrides import overrides,final,EnforceOverrides
 
-if sys.version >= '3':
-    import unittest
-    from overrides import overrides,final,EnforceOverrides
+class Enforcing(EnforceOverrides):
 
-    class Enforcing(EnforceOverrides):
+    @final
+    def finality(self):
+        return "final"
 
-        @final
-        def finality(self):
-            return "final"
+    def nonfinal1(self):
+        return "super1"
 
-        def nonfinal1(self):
-            return "super1"
+    def nonfinal2(self):
+        return "super2"
 
-        def nonfinal2(self):
-            return "super2"
-    
-    class EnforceTests(unittest.TestCase):
+class EnforceTests(unittest.TestCase):
 
-        def test_enforcing_when_all_ok(self):
-            class Subclazz(Enforcing):
-                @overrides
-                def nonfinal1(self):
-                    return 1
-            sc = Subclazz()
-            self.assertEqual(sc.finality(), "final")
-            self.assertEqual(sc.nonfinal1(), 1)
-            self.assertEqual(sc.nonfinal2(), "super2")
-    
-        def tests_enforcing_when_finality_broken(self):
-            try:
-                class BrokesFinality(Enforcing):
-                    def finality(self):
-                        return "NEVER HERE"
-                raise RuntimeError('Should not go here')
-            except AssertionError:
-                pass
-    
-        def test_enforcing_when_none_explicit_override(self):
-            try:
-                class Overrider(Enforcing):
-                    def nonfinal2(self):
-                        return "NEVER HERE EITHER"
-                raise RuntimeError('Should not go here')
-            except AssertionError:
-                pass
+    def test_enforcing_when_all_ok(self):
+        class Subclazz(Enforcing):
+            @overrides
+            def nonfinal1(self):
+                return 1
+        sc = Subclazz()
+        self.assertEqual(sc.finality(), "final")
+        self.assertEqual(sc.nonfinal1(), 1)
+        self.assertEqual(sc.nonfinal2(), "super2")
+
+    def tests_enforcing_when_finality_broken(self):
+        try:
+            class BrokesFinality(Enforcing):
+                def finality(self):
+                    return "NEVER HERE"
+            raise RuntimeError('Should not go here')
+        except AssertionError:
+            pass
+
+    def test_enforcing_when_none_explicit_override(self):
+        try:
+            class Overrider(Enforcing):
+                def nonfinal2(self):
+                    return "NEVER HERE EITHER"
+            raise RuntimeError('Should not go here')
+        except AssertionError:
+            pass
 
