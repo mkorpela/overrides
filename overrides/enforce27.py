5b02dfd86d0cac1bb8e4d0dcd80ce7a621dc6a27
Tudor Masek <tmasek@gmail.com>
2020-02-05 14:42:10 -0800

    keep up with upstream/master

--- overrides/enforce27.py
+++ overrides/enforce27.py
@@ -10,10 +10,11 @@ class EnforceOverridesMeta(ABCMeta):
             # otherwise the error would have emerged during the parent class checking
             if name.startswith('__'):
                 continue
+            value = mcls.handle_special_value(value)
             is_override = getattr(value, '__override__', False)
             for base in bases:
                 base_class_method = getattr(base, name, False)
-                if not base_class_method:
+                if not base_class_method or not callable(base_class_method):
                     continue
                 assert is_override, \
                     '%s method %s overrides but does not have @overrides decorator' % (
@@ -23,6 +24,15 @@ class EnforceOverridesMeta(ABCMeta):
                     '%s method %s is finalized in %s, it cannot be overridden' % (
                         cls_name, base_class_method, base)
         return cls
+
+    @staticmethod
+    def handle_special_value(value):
+        if isinstance(value, classmethod):
+            value = value.__get__(None, dict)
+        elif isinstance(value, property):
+            value = value.fget
+        return value
+
 
 class EnforceOverrides(object):
     "Use this as the parent class for your custom classes"
