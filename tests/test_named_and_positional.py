ac8012f527b45d680f0c6a5f595f2dac0fb620c1
Mikko Korpela <mikko.korpela@gmail.com>
2021-04-27 21:39:58 +0300

    add failing test

---
+++ tests/test_named_and_positional.py
@@ -0,0 +1,15 @@
+from overrides import overrides, EnforceOverrides
+
+class A(EnforceOverrides):
+
+   def methoda(self, x=0):
+       print(x)
+
+
+def test_should_pass():
+    class B(A):
+
+        @overrides
+        def methoda(self, y=1, **kwargs):
+            print(y)
+            super().methoda(**kwargs)
