020ccd0006aaa6d960e791ec5268b19480653e2e
Mikko Korpela <mikko.korpela@gmail.com>
2021-04-28 20:17:48 +0300

    more tests

--- tests/test_named_and_positional.py
+++ tests/test_named_and_positional.py
@@ -12,3 +12,24 @@ def test_should_pass():
         def methoda(self, y=1, **kwargs):
             print(y)
             super().methoda(**kwargs)
+
+def test_can_not_override_with_positional_only():
+    try:
+        class C(A):
+            @overrides
+            def methoda(self, x=0, /):
+                pass
+        raise AssertionError("Should not go here")
+    except TypeError:
+        pass
+
+def test_can_not_override_with_keyword_only():
+    try:
+        class C2(A):
+            @overrides
+            def methoda(self, *, x=0):
+                pass
+
+        raise AssertionError("Should not go here")
+    except TypeError:
+        pass
