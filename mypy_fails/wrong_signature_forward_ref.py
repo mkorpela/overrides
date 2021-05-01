c3c3f7394f9f2492bdd58cad4e24a084a44f1f7d
Mikko Korpela <mikko.korpela@gmail.com>
2021-05-01 22:53:07 +0300

    make them compatible with python 3.6

--- mypy_fails/wrong_signature_forward_ref.py
+++ mypy_fails/wrong_signature_forward_ref.py
@@ -1,4 +1,3 @@
-from __future__ import annotations
 from overrides import overrides
 
 
@@ -9,5 +8,5 @@ class Parent:
 
 class Child(Parent):
     @overrides
-    def metoda(self) -> Child:
+    def metoda(self) -> 'Child':
         return self
