ae1a96aaeca7b036704d8421c611f05eb2e8ef00
Mikko Korpela <mikko.korpela@gmail.com>
2021-05-01 21:03:04 +0300

    3.8

--- conftest.py
+++ conftest.py
@@ -4,6 +4,6 @@ from typing import Optional
 import py
 
 def pytest_ignore_collect(path: py.path.local, config: "Config") -> Optional[bool]:
-    if sys.version_info[0] == 3 and sys.version_info[1] < 7:
-        if str(path).endswith("__py37.py"):
+    if sys.version_info[0] == 3 and sys.version_info[1] < 8:
+        if str(path).endswith("__py38.py"):
             return True
