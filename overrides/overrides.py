a5c2d92c54cf4bf3cead186900c372ec27ef4562
Mikko Korpela <mikko.korpela@gmail.com>
2021-05-06 22:23:22 +0300

    Bump version to 6.0.1

--- overrides/overrides.py
+++ overrides/overrides.py
@@ -21,7 +21,7 @@ import sys
 from types import FunctionType
 from typing import Any, Callable, List, Optional, Tuple, TypeVar, Union, overload
 
-__VERSION__ = "6.0.0"
+__VERSION__ = "6.0.1"
 
 from overrides.signature import ensure_signature_is_compatible
 
