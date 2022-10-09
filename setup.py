fae73ff777b5b66237c98366bda7ca41ce42bbcf
Mikko Korpela <mikko.korpela@gmail.com>
2022-10-09 18:42:01 +0300

    Bump version to 7.0.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(join(CURDIR, "README.rst")) as f:
 
 setup(
     name="overrides",
-    version="6.5.0",
+    version="7.0.0",
     description=desc,
     long_description=LONG_DESCRIPTION,
     author=name,
